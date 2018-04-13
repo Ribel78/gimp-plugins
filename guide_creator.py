7ce8c20bed46c310e75e8c927b9530ec3595d8ce
Ribel78 <rbelev@abv.bg>
2018-04-14 02:34:36 +0300

    Update guide_creator.py

--- guide_creator.py
+++ guide_creator.py
@@ -22,21 +22,16 @@
 
 
 # Usage:
-# Gudes can be created using selected option from "Add guides from:" menu
+# Gudes can be created using "Add guides from:" menu.
 # "Formula" option:
-# You can add gudes by describig them using text.
-# H for horisontal (-) gudes and V for vertical (|) gudes followed by numbers (indegers or floats)
-# describing guide's position starting from the top left corner of the image.
-# Position can be percent or pixels, depending on the option "Use percent/pixels"
-# Example: H25V25H50
-# Another use of formula is equidistant guide distribution.
-# in this case use only letters to indicate the number of guides of different type.
+# You can add gudes by describig them using text. H for horisontal (-) gudes and V for vertical (|) gudes followed by numbers (indegers or floats) describing guide's position starting from the top left corner of the image. Position can be percent or pixels, depending on the toggle "Use percent/pixels".
+# Example: H25V36.4H50
+# Another use of "Formula" is equidistant guide distribution. in this case use only letters to indicate the number of guides of different type.
 # Example: HHHVVVVV
 # Avoid spaces or other letters in the formula for acurate results.
 # "Include mirrored guides" toggle can add also guides that are symmetrical to those from the formula.
 # "Remove previous guides" toggle can clear existing guides before adding the new ones.
-# The rest of the oprions use active layer or selection bounds as reference.
-
+# The rest of the options use active layer or selection bounds as reference for adding new guides.
 
 from gimpfu import *
 import re
